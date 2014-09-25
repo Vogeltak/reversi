0c0aa49a4457d834608abaef0417ee8f84e76b9d
Vogeltak <vogeltak@gmail.com>
2014-09-25 18:44:54 +0200

    Init ComputerPlayer.js

---
+++ www/scripts/ComputerPlayer.js
@@ -0,0 +1,10 @@
+/*
+ *
+ *	File: ComputerPlayer.js
+ *	Description: Computer player class for instantiating ComputerPlayer objects
+ *
+ *	Authors: Vogeltak, Pacmega, Joep359
+ *
+ *	09/25/2014
+ *
+ */
