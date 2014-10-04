0af4fe6e76c356f74d6314d478666295ffbbac99
Vogeltak <vogeltak@gmail.com>
2014-10-04 11:25:31 +0200

    Added initialisation
    Initialisation will start when the start button is clicked. The standard board state will get displayed.

--- www/scripts/Game.js
+++ www/scripts/Game.js
@@ -1,7 +1,7 @@
 /*
  *
  *	File: Game.js
- *	Description: Main file for setting up and maintaining the game
+ *	Description: Main file for setti#ng up and maintaining the game
  *
  *	Authors: Vogeltak, Pacmega, Joep359
  *
@@ -9,9 +9,30 @@
  *
  */
 
-// initialize empty board array
-var board = [];
+document.getElementsByClassName('button')[0].onclick = init;
 
-for (var i = 0; i < 64; i++) {
-	board[i] = new Tile(i);
+var board = [];
+	
+function init() {
+	// initialize empty board array
+	/*
+	 *	BOARD LAYOUT
+	 *	0  1  2  3  4  5  6  7
+	 *	8  9  10 11 12 13 14 15
+	 *	16 17 18 19 20 21 22 23
+	 *	24 25 26 27 28 29 30 31
+	 *	32 33 34 35 36 37 38 39
+	 *	40 41 42 43 44 45 46 47
+	 *	48 49 50 51 52 53 54 55
+	 *	56 57 58 59 60 61 62 63
+	 */
+
+	for (var i = 0; i < 64; i++) {
+		board[i] = new Tile(i);
+	}
+
+	board[27].setGreen();
+	board[28].setGreen();
+	board[35].setRed();
+	board[36].setRed();
 }
--- www/scripts/Tile.js
+++ www/scripts/Tile.js
@@ -9,7 +9,28 @@
  *
  */
 
- var Tile = function(index) {
- 	Tile.prototype.index = index;
- }
- 
+/* Tile colors
+ * neutral : #ddd
+ * green : #2ecc71
+ * green-hover : #27ae60
+ * red : #e74c3c
+ * red-hover : #c0392b
+ */
+
+function Tile(index) {
+	
+	this.index = index;
+
+	this.getTile = function() {
+		return document.getElementsByClassName(this.index)[0];
+	}
+
+	this.setGreen = function() {
+		this.getTile().style.backgroundColor = '#2ecc71';
+	}
+
+	this.setRed = function() {
+		this.getTile().style.backgroundColor = '#e74c3c';
+	}
+
+}
