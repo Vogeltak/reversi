e7e3184b6ea8e96ae37222c0f644ea1c6da3ee6c
unknown <96009078@G008-D01.theresialyceum.local>
2014-10-06 09:17:00 +0200

    Init Board.js

---
+++ www/scripts/Board.js
@@ -0,0 +1,11 @@
+/*
+ *
+ *	File: Board.js
+ *	Description: Board class for instantiating Board objects. 
+ *		     A Board object can handle every change is made to the state of the board
+ *
+ *	Authors: Vogeltak, Pacmega, Joep359
+ *
+ *	10/06/2014
+ *
+ */
