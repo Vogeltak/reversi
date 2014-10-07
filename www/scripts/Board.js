94c547e9fbf64499a7a60126d503db6925d32ef0
unknown <96009078@E001-W02.theresialyceum.local>
2014-10-07 09:15:01 +0200

    Started implementing Board.js
    It does not work yet, I do not know why though..

--- www/scripts/Board.js
+++ www/scripts/Board.js
@@ -2,10 +2,46 @@
  *
  *	File: Board.js
  *	Description: Board class for instantiating Board objects. 
- *		     A Board object can handle every change is made to the state of the board
+ *		     	 A Board object can handle every change that is made to the state of the board
  *
  *	Authors: Vogeltak, Pacmega, Joep359
  *
  *	10/06/2014
  *
  */
+
+function Board() {
+
+	// initialize empty board array
+	/*
+	 *	BOARD LAYOUT
+	 *	0  1  2  3  4  5  6  7
+	 *	8  9  10 11 12 13 14 15
+	 *	16 17 18 19 20 21 22 23
+	 *	24 25 26 27 28 29 30 31
+	 *	32 33 34 35 36 37 38 39
+	 *	40 41 42 43 44 45 46 47
+	 *	48 49 50 51 52 53 54 55
+	 *	56 57 58 59 60 61 62 63
+	 */
+
+	this.board = [];
+
+	for (var i = 0; i < 64; i++) {
+		board[i] = new Tile(i);
+		// set each tile color to neutral
+		board[i].setNeutral();
+	}
+
+	// set default state
+	board[27].setRed();
+	board[28].setRed();
+	board[35].setGreen();
+	board[36].setGreen();
+
+	// set event listener for every tile
+	for (var j = 0; j < 64; j++) {
+		board[j].getTile().addEventListener('click', board[j], false);
+	}
+
+}
--- www/scripts/Game.js
+++ www/scripts/Game.js
@@ -12,50 +12,23 @@
 document.getElementsByClassName('button')[0].onclick = init;
 document.getElementsByClassName('reset')[0].onclick = init;
 
-var board = [];
+var board;
 
 var playerColor = '#2ecc71';
 
-document.getElementsByClassName('human')[0].addEventListener('click', function() { playerColor = '#2ecc71'; });
-document.getElementsByClassName('computer')[0].addEventListener('click', function() { playerColor = '#e74c3c'; });
+document.getElementsByClassName('human')[0].addEventListener('click', function() { playerColor = '#2ecc71'; console.log('Set tile color to ' +  playerColor); });
+document.getElementsByClassName('computer')[0].addEventListener('click', function() { playerColor = '#e74c3c'; console.log('Set tile color to ' +  playerColor); });
 	
 function init() {
-	// set visibility of board to 'visible'
+	// set display of board to 'inline-block'
 	document.getElementById('board').style.display = 'inline-block';
 
-	// set visibility of settings to 'visible'
+	// set display of settings to 'inline-block'
 	document.getElementById('settings').style.display = 'inline-block';
 
-	// set visibility start to 'hidden'
+	// set display start to 'none'
 	document.getElementsByClassName('button')[0].style.display = 'none';
+	
+	board = new Board();
 
-	// initialize empty board array
-	/*
-	 *	BOARD LAYOUT
-	 *	0  1  2  3  4  5  6  7
-	 *	8  9  10 11 12 13 14 15
-	 *	16 17 18 19 20 21 22 23
-	 *	24 25 26 27 28 29 30 31
-	 *	32 33 34 35 36 37 38 39
-	 *	40 41 42 43 44 45 46 47
-	 *	48 49 50 51 52 53 54 55
-	 *	56 57 58 59 60 61 62 63
-	 */
-
-	for (var i = 0; i < 64; i++) {
-		board[i] = new Tile(i);
-		// set each tile color to neutral
-		board[i].setNeutral();
-	}
-
-	// set default state
-	board[27].setRed();
-	board[28].setRed();
-	board[35].setGreen();
-	board[36].setGreen();
-
-	// set event listener for every tile
-	for (var j = 0; j < 64; j++) {
-		board[j].getTile().addEventListener('click', board[j], false);
-	}
 }
