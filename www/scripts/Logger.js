0785b16ac11da6a078b8432a4ed2c9f72dec9d16
Vogeltak <vogeltak@gmail.com>
2014-10-15 14:17:13 +0200

    Init Logger.js

---
+++ www/scripts/Logger.js
@@ -0,0 +1,10 @@
+/*
+ *
+ *	File: Logger.js
+ *	Description: Logger class for instantiating Logger objects, used to log information to the player.
+ *
+ *	Authors: Vogeltak, Pacmega, Joep359
+ *
+ *	09/25/2014
+ *
+ */
